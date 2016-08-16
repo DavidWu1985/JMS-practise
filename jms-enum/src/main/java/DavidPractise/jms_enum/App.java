fa0c266046915e5168579c521b16d8e02a2d5483
David.Wu <david.wu@kutesmart.com>
2016-08-16 10:05:57 +0800

    jms practise init commit

---
+++ jms-enum/src/main/java/DavidPractise/jms_enum/App.java
@@ -0,0 +1,13 @@
+package DavidPractise.jms_enum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
